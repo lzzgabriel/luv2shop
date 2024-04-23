00c6c8da0c337eff35b081dfb78ef250785873e9
lzzgabriel <gabrielhlezo@gmail.com>
2024-04-23 08:38:36 -0300

    Prepare for use okta authentication

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oaglp3skhaSMrYJU5d7',
+        issuer: 'https://dev-68385812.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
