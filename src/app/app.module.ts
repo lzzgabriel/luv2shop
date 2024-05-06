d8972f2eadbcb555ee07e07fcd9c15507e2cd78e
lzzgabriel <gabrielhlezo@gmail.com>
2024-05-06 09:13:26 -0300

    Add route guarding for members page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductService } from './services/product.service';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
@@ -16,12 +16,17 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginStatusComponent } from './components/login-status/login-status.component';
 import myAppConfig from './config/my-app-config';
-import { provideAuth0 } from '@auth0/auth0-angular';
+import { AuthGuard, AuthService, provideAuth0 } from '@auth0/auth0-angular';
 import { MembersPageComponent } from './components/members-page/members-page.component';
 
 const oktaConfig = myAppConfig.oidc;
 
+function sendToLoginPage(auth: AuthService) {
+  auth.loginWithRedirect();
+}
+
 const routes: Routes = [
+  {path: 'members', component: MembersPageComponent, canActivate: [AuthGuard], data: {onAuthRequired: sendToLoginPage}},
   {path: 'checkout', component: CheckoutComponent},
   {path: 'cart-details', component: CartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
