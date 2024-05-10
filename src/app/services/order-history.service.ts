696f5971d7117eb8af4fd56fb7a750a5049ebdc8
lzzgabriel <gabrielhlezo@gmail.com>
2024-05-10 08:40:56 -0300

    Add order history service

---
+++ src/app/services/order-history.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OrderHistory } from '../common/order-history';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderHistoryService {
+
+  private orderUrl = 'http://localhost:8080/api/orders'
+
+  constructor(private httpClient: HttpClient) { }
+
+  getOrderHistory(email: String): Observable<GetResponseOrderHistory> {
+    const url = `${this.orderUrl}/search/findByCustomerEmail?email${email}`
+
+    return this.httpClient.get<GetResponseOrderHistory>(this.orderUrl)
+  }
+}
+
+interface GetResponseOrderHistory {
+  _embedded: {
+    orders: OrderHistory[]
+  }
+}
