f815c2134152d87ab6ecebc2fc3eb72756a95a78
lzzgabriel <gabrielhlezo@gmail.com>
2024-05-10 08:35:20 -0300

    Update login status component

--- src/app/components/login-status/login-status.component.ts
+++ src/app/components/login-status/login-status.component.ts
@@ -1,6 +1,6 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
-import { AuthService } from '@auth0/auth0-angular';
+import { AuthService, User } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-login-status',
@@ -9,12 +9,17 @@ import { AuthService } from '@auth0/auth0-angular';
 })
 export class LoginStatusComponent implements OnInit {
 
+  storage: Storage = sessionStorage
+
   constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
 
   ngOnInit(): void {
   }
 
   getUserDetails() {
+    if (this.auth.isAuthenticated$) {
+      this.storage.setItem('user_email', JSON.stringify((this.auth.user$ as User).email))
+    }
   }
 
   logout() {
