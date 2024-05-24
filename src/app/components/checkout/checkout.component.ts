1cbb4fd86dca4b6fd55b7ec28dd021347b7e0be2
lzzgabriel <gabrielhlezo@gmail.com>
2024-05-24 11:54:58 -0300

    Bug fix: remove old cart data in storage

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -219,6 +219,7 @@ export class CheckoutComponent implements OnInit {
     this.cartService.cartItems = [];
     this.cartService.totalPrice.next(0);
     this.cartService.totalQuantity.next(0);
+    this.cartService.persistCartItems();
     
     this.checkoutFormGroup.reset();
 
